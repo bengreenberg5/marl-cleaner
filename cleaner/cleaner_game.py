d7cff563f01344eefa304cd681e89be26a34e1b9
anvil <bengreenberg5@gmail.com>
2021-08-24 12:03:55 -0400

    add penalties to config

--- cleaner/cleaner_game.py
+++ cleaner/cleaner_game.py
@@ -5,11 +5,13 @@ from cleaner.utils import *
 
 
 class CleanerGame:
-    def __init__(self, layout, tick_limit, num_agents, agent_names):
+    def __init__(self, layout, tick_limit, num_agents, agent_names, time_penalty=-0.25, clean_penalty=0.0):
         self.layout = layout  # human-readable layout
         self.tick_limit = tick_limit  # how many time steps before game ends
         self.num_agents = num_agents  # how many agents on the board
         self.agent_names = agent_names  # list of agent identifiers
+        self.time_penalty = time_penalty  # negative reward for each timestep
+        self.clean_penalty = clean_penalty  # negative reward for moving into a clean square
         self.size = (len(layout), len(layout[0]))  # height and width of grid
         self.reset()
 
@@ -59,7 +61,7 @@ class CleanerGame:
         return obs
 
     def step(self, actions):
-        reward = -0.25
+        reward = self.time_penalty
         for agent, action in actions.items():
             pos = self.agent_pos[agent]
             new_pos = pos + MOVES[action]
@@ -67,7 +69,9 @@ class CleanerGame:
                 continue
             self.grid["agent"][pos] = 0
             self.grid["agent"][new_pos] = 1
-            if self.grid["dirty"][new_pos]:
+            if self.grid["clean"][new_pos]:
+                reward += self.clean_penalty
+            elif self.grid["dirty"][new_pos]:
                 self.grid["dirty"][new_pos] = 0
                 self.grid["clean"][new_pos] = 1
                 reward += 1
