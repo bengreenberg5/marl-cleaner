1b49cd94f323655f3f4cbf78ab9d00b3a2619f9d
anvil <bengreenberg5@gmail.com>
2021-09-15 00:06:27 -0400

    add tests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Custom implementation of the Cleaner Game described [here](https://github.com/Bigpig4396/Multi-Agent-Reinforcement-Learning-Environment/blob/master/env_Cleaner/Cleaner.pdf).
 
-This work was completed as part of SERI's Summer Research Program. The repo is still in progress as of 2021-09-10 -- by 2021-09-14, I will hopefully have added tests, typing, requirements, documentation, and a more fleshed-out README.
+This work was completed as part of SERI's Summer Research Program. The repo is still in progress as of 2021-09-15 -- by 2021-09-20, I will hopefully have added tests, typing, requirements, documentation, and a more fleshed-out README.
--- test_cleaner.py
+++ test_cleaner.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cleaner.utils import load_config
+from cleaner.utils import load_config, MASKS
 from cleaner.cleaner_env import CleanerEnv
 
 
@@ -8,12 +8,96 @@ from cleaner.cleaner_env import CleanerEnv
 def env():
     config = load_config("simple_2")
     env = CleanerEnv(config["env_config"], "test", None)
+    print(env.game.get_agent_obs())
     return env
 
 
 def test_create(env):
     assert isinstance(env, CleanerEnv)
     env.game.validate_grid()
+    for layer in MASKS.keys():
+        assert layer in env.game.grid
 
 
-# TODO
+def test_step(env):
+    assert env.game.tick == 0
+    assert env.game.agent_pos["test:0"] == (1, 1)
+    assert env.game.agent_pos["test:1"] == (11, 11)
+    env.step({
+        "test:0": 4,  # east
+        "test:1": 3,  # west
+    })
+    assert env.game.tick == 1
+    assert env.game.agent_pos["test:0"] == (1, 2)
+    assert env.game.agent_pos["test:1"] == (11, 10)
+    env.step({
+        "test:0": 2,  # south
+        "test:1": 1,  # north
+    })
+    assert env.game.tick == 2
+    assert env.game.agent_pos["test:0"] == (2, 2)
+    assert env.game.agent_pos["test:1"] == (10, 10)
+    env.step({
+        "test:0": 3,  # west
+        "test:1": 4,  # east
+    })
+    assert env.game.tick == 3
+    assert env.game.agent_pos["test:0"] == (2, 1)
+    assert env.game.agent_pos["test:1"] == (10, 11)
+    env.reset()
+    assert env.game.tick == 0
+    assert env.game.agent_pos["test:0"] == (1, 1)
+    assert env.game.agent_pos["test:1"] == (11, 11)
+
+
+def test_boundaries(env):
+    actions = [{"test:0": i, "test:1": i} for i in range(5)]
+    for action in actions:
+        for i in range(100):
+            env.step(action)
+
+
+def test_cleaning(env):
+    assert env.game.grid["clean"].sum().sum() == 2
+    _, reward, _, _ = env.step({"test:0": 4, "test:1": 0})
+    assert sum(reward.values()) == 0.75
+    assert env.game.grid["clean"].sum().sum() == 3
+    _, reward, _, _ = env.step({"test:0": 0, "test:1": 3})
+    assert sum(reward.values()) == 0.75
+    assert env.game.grid["clean"].sum().sum() == 4
+    _, reward, _, _, = env.step({"test:0": 0, "test:1": 0})
+    assert sum(reward.values()) == -0.25
+    assert env.game.grid["clean"].sum().sum() == 4
+    _, reward, _, _ = env.step({"test:0": 4, "test:1": 3})
+    assert sum(reward.values()) == 1.75
+    assert env.game.grid["clean"].sum().sum() == 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
